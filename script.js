e612460d7424cc7bda9cec07c3b405debb68d562
ankitkumar0008 <96786356+ankitkumar0008@users.noreply.github.com>
2025-10-25 13:30:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 //your code here!
 
+let ol = document.getElementById('infi-list');
+
+let count = 1;
+
+function addItem(num) {
+for(let i=0; i < num; i++) {
+
+let li = document.createElement('li');
+
+li.textContent = `Item ${count}`;
+
+ol.appendChild(li);
+
+count++;
+}
+
+}
+addItem(10);
+ ol.addEventListener('scroll', () => {
+
+if (ol.scrollTop+ ol.clientHeight >= ol.scrollHeight) {
+
+}
+
+addItem(2);
+
+});
